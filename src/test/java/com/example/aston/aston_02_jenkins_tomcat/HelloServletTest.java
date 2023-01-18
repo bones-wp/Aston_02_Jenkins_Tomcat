321d6d0e88d80b818813126370329eaace1c9e0d
j-thrash <j-thrash@ya.ru>
2023-01-18 03:00:36 +0300

    Test add

---
+++ src/test/java/com/example/aston/aston_02_jenkins_tomcat/HelloServletTest.java
@@ -0,0 +1,12 @@
+package com.example.aston.aston_02_jenkins_tomcat;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloServletTest {
+
+    @Test
+    void doGet() {
+    }
+}
